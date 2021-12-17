0c04addab0eaa2abe1161b7f081ae6d782885003
Antoniatch <antoniatch@zohomail.eu>
2021-12-17 10:16:28 +0100

    Admin page with link to Add new house

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+function Footer() {
+    return (
+        <Navbar>
+            <p> Contact </p>
+            <p> Mentions légales </p>
+        </Navbar>
+    )
+}
+
+const Navbar = styled.nav`
+    display: flex;
+    background: #5d7b4c;
+    height: 5rem;
+    font-family: 'Trebuchet MS';
+    color: white;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 3rem;
+`
+
+export default Footer;
---
+++ src/components/styled-components/colors.js
@@ -0,0 +1,8 @@
+const colors = {
+    green : '#5d7b4c',
+    blue : '#1c2c46',
+    yellow : '#eeeb8f',
+    brown : '#ba9b5c'
+}
+
+export default colors;
