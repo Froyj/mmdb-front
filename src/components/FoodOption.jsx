99a21fda1cce34b625635ff12e9e2787b478e7cf
Maxime MEDAL <medal.max@gmail.com>
2022-01-24 14:11:58 +0100

    checkbox

--- src/components/FoodOption.jsx
+++ src/components/FoodOption.jsx
@@ -7,33 +7,34 @@ const FoodOption = ({ repas }) => {
     repas: PropTypes.string.isRequired,
   };
   return (
-  <FoodContainer>
-    <BreakfastTitle>{repas.name}</BreakfastTitle>
-    <DishName>{repas.dish.map((name)=><option type ="checkbox">
-      {`${name.name} : ${name.price}`} 
-      </option>
-       )}</DishName>
-  </FoodContainer>
-  )
+    <FoodContainer>
+      <BreakfastTitle>{repas.name}</BreakfastTitle>
+      <DishName>
+        <select>
+          {repas.dish.map((name) => (
+            <option type="checkbox">{`${name.name} : ${name.price}`}</option>
+          ))}
+        </select>
+      </DishName>
+    </FoodContainer>
+  );
 };
 
 const FoodContainer = styled.div`
   display: flex;
   flex-direction: column;
-
 `;
 
-const BreakfastTitle = styled.h1`
-`;
+const BreakfastTitle = styled.h1``;
 
 const DishName = styled.ul`
-display : flex;
-flex-direction: column;
-line-height: 20px;
-  
-input {
-  margin-right: 10px;
-}
+  display: flex;
+  flex-direction: column;
+  line-height: 20px;
+
+  input {
+    margin-right: 10px;
+  }
 `;
 
 export default FoodOption;
