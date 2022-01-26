ea6185793c6a1edef8b6d3e5d9a414329637155d
MalikCherfi <malikcherfi@gmail.com>
2022-01-26 18:00:40 +0100

    remise en place des routes pour les formulaires

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import { Routes, Route } from 'react-router-dom';
-import { useState, useEffect, useReducer } from 'react';
+import { Routes, Route } from "react-router-dom";
+import { useState, useEffect, useReducer } from "react";
 
 import getHouses from "./data/houses";
 import getBookings from "./data/bookings";
 
-import Navigation from './components/Navigation';
-import Footer from './components/Footer';
-import About from './pages/About';
-import AddNewHouse from './pages/AddNewHouse';
-import Admin from './pages/Admin';
-import Home from './pages/Home';
-import House from './pages/House';
-import Search from './pages/Search';
-import Services from './pages/Services';
-import UserProfile from './pages/UserProfile';
-import ConnectionModal from './components/ConnectionModal';
-import SignUpForm from './components/SignUpForm';
-import PrivateRoute from './components/auth/PrivateRoute';
-import { ADMIN } from './constants/roles';
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+import About from "./pages/About";
+import AddNewHouse from "./pages/AddNewHouse";
+import UpdateHouse from "./pages/UpdateHouse";
+import Admin from "./pages/Admin";
+import Home from "./pages/Home";
+import House from "./pages/House";
+import Search from "./pages/Search";
+import Services from "./pages/Services";
+import UserProfile from "./pages/UserProfile";
+import ConnectionModal from "./components/ConnectionModal";
+import SignUpForm from "./components/SignUpForm";
+import PrivateRoute from "./components/auth/PrivateRoute";
+import { ADMIN } from "./constants/roles";
 import {
   initialState,
   userContextReducer,
-} from './reducers/userContextReducer';
-import { UserContextProvider } from './contexts/user';
+} from "./reducers/userContextReducer";
+import { UserContextProvider } from "./contexts/user";
 
 function App() {
   const [houses, setHouses] = useState([]);
@@ -37,36 +38,41 @@ function App() {
   if (houses) {
     return (
       <>
-        <UserContextProvider value={{...userContext, dispatch}}>
+        <UserContextProvider value={{ ...userContext, dispatch }}>
           <Navigation />
 
-     
-            <Routes>
-              {/* Connected User */}
-              <Route path='/profil' element={<UserProfile />} />
-              {/* Auth Routes */}
-              <Route path='/se-connecter' element={<ConnectionModal />} />
-              <Route path='/creation-compte' element={<SignUpForm />} />
-              {/* Public Route */}
-              <Route exact path='/' element={<Home />} />
-              <Route path='/services' element={<Services />} />
-              <Route path='/qui-sommes-nous' element={<About />} />
-              <Route
-                path='/nos-maisons-forestieres'
-                element={<Search houses={houses} />}
-              />
-              <Route path='/maison/:id' element={<House houses={houses} />} />
+          <Routes>
+            {/* Connected User */}
+            <Route path="/profil" element={<UserProfile />} />
+            {/* Auth Routes */}
+            <Route path="/se-connecter" element={<ConnectionModal />} />
+            <Route path="/creation-compte" element={<SignUpForm />} />
+            {/* Public Route */}
+            <Route exact path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/qui-sommes-nous" element={<About />} />
+            <Route
+              path="/nos-maisons-forestieres"
+              element={<Search houses={houses} />}
+            />
+            <Route path="/maison/:id" element={<House houses={houses} />} />
 
-              {/* Admin Routes */}
-              <Route path='/admin' element={<PrivateRoute role={ADMIN} />}>
-                <Route
-                  path='dashboard'
-                  element={<Admin houses={houses} bookings={bookings} />}
-                />
-                <Route path='maison/ajouter' element={<AddNewHouse />} />
-              </Route>
-            </Routes>
-  
+            {/* Admin Routes */}
+            <Route path="/admin" element={<PrivateRoute role={ADMIN} />}>
+              <Route
+                path="dashboard"
+                element={<Admin houses={houses} bookings={bookings} />}
+              />
+              <Route
+                path="dashboard/maison/ajouter"
+                element={<AddNewHouse />}
+              />
+              <Route
+                path="dashboard/mise-a-jour-maison/:id"
+                element={<UpdateHouse />}
+              />
+            </Route>
+          </Routes>
 
           <Footer />
         </UserContextProvider>
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,44 +1,45 @@
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import styled from "styled-components";
 
-import Section from '../components/styled-components/Section';
-import FilledButton from '../components/styled-components/FilledButton';
-import Global from '../components/styled-components/Global';
+import Section from "../components/styled-components/Section";
+import FilledButton from "../components/styled-components/FilledButton";
+import Global from "../components/styled-components/Global";
 
-import AdminHouseList from '../components/AdminHouseList';
-import AdminReservationList from '../components/AdminReservationList';
+import AdminHouseList from "../components/AdminHouseList";
+import AdminReservationList from "../components/AdminReservationList";
 
 function Admin({ houses, bookings }) {
-        Admin.propTypes = {
-        houses: PropTypes.arrayOf(PropTypes.object).isRequired,
-        bookings: PropTypes.arrayOf(PropTypes.object).isRequired,
-    }
+  Admin.propTypes = {
+    houses: PropTypes.arrayOf(PropTypes.object).isRequired,
+    bookings: PropTypes.arrayOf(PropTypes.object).isRequired,
+  };
 
-    return (
-        <Global>
-            <Section>
-                <h1>Statistiques</h1>
-            </Section>
-            <Section>
-                <h1>Mes maisons des bois</h1>
-            </Section>
-            <AdminHouseList houses={houses} />
-            <ButtonsDiv>
-                <Link to='maison/ajouter' ><FilledButton> Ajouter une nouvelle maison </FilledButton></Link>
-                <FilledButton> Supprimer une maison </FilledButton>
-            </ButtonsDiv>
-            <Section>
-                <h1>Mes réservations</h1>
-            </Section>
-            <AdminReservationList bookings={bookings} />
-        </Global>
-    );
+  return (
+    <Global>
+      <Section>
+        <h1>Statistiques</h1>
+      </Section>
+      <Section>
+        <h1>Mes maisons des bois</h1>
+      </Section>
+      <AdminHouseList houses={houses} />
+      <ButtonsDiv>
+        <Link to="maison/ajouter">
+          <FilledButton> Ajouter une nouvelle maison </FilledButton>
+        </Link>
+      </ButtonsDiv>
+      <Section>
+        <h1>Mes réservations</h1>
+      </Section>
+      <AdminReservationList bookings={bookings} />
+    </Global>
+  );
 }
 
 const ButtonsDiv = styled.div`
-    display: flex;
-    margin: 1rem;
-`
+  display: flex;
+  margin: 1rem;
+`;
 
 export default Admin;
