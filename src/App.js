8b24ef68d87a267f3346cb0d99ac93aec3a4e23e
Geoffroy Periot <geoffroy.periot.dev@gmail.com>
2022-01-27 14:11:03 +0100

    login attempt on application loading

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import { useState, useEffect, useReducer } from 'react';
+import axios from './helper/axios-config';
 
 import getHouses from './data/houses';
 import getBookings from './data/bookings';
@@ -29,6 +30,27 @@ function App() {
   const [bookings, setBookings] = useState([]);
   const [userContext, dispatch] = useReducer(userContextReducer, initialState);
 
+  function loginAttempt() {
+    axios
+      .get('/auth/check-token')
+      .then((res) => res.data)
+      .then((user) => {
+        dispatch({
+          type: 'CONNECTION',
+          payload: { userId: user.id, roleId: user.role_id },
+        });
+      })
+      .catch((err) => {
+        console.log(err)
+      });
+  }
+
+  useEffect(() => {
+    if (!userContext.isConnected) {
+      loginAttempt();
+    }
+  }, []);
+
   useEffect(() => {
     getHouses(setHouses);
     getBookings(setBookings);
@@ -38,37 +60,33 @@ function App() {
   if (houses) {
     return (
       <>
-        <UserContextProvider value={{...userContext, dispatch}}>
+        <UserContextProvider value={{ ...userContext, dispatch }}>
           <Navigation />
+          <Routes>
+            {/* Connected User */}
+            <Route path='/Profil' element={<UserProfile />} />
+            {/* Auth Routes */}
+            <Route path='/SeConnecter' element={<ConnectionModal />} />
+            <Route path='/CreationCompte' element={<SignUpForm />} />
+            {/* Public Route */}
+            <Route exact path='/' element={<Home />} />
+            <Route path='/Services' element={<Services />} />
+            <Route path='/QuiSommesNous' element={<About />} />
+            <Route
+              path='/NosMaisonsForestieres'
+              element={<Search houses={houses} />}
+            />
+            <Route path='/Maison/:id' element={<House houses={houses} />} />
 
-          <Global>
-            <Routes>
-              {/* Connected User */}
-              <Route path='/Profil' element={<UserProfile />} />
-              {/* Auth Routes */}
-              <Route path='/SeConnecter' element={<ConnectionModal />} />
-              <Route path='/CreationCompte' element={<SignUpForm />} />
-              {/* Public Route */}
-              <Route exact path='/' element={<Home />} />
-              <Route path='/Services' element={<Services />} />
-              <Route path='/QuiSommesNous' element={<About />} />
+            {/* Admin Routes */}
+            <Route path='/admin' element={<PrivateRoute role={ADMIN} />}>
               <Route
-                path='/NosMaisonsForestieres'
-                element={<Search houses={houses} />}
+                path='dashboard'
+                element={<Admin houses={houses} bookings={bookings} />}
               />
-              <Route path='/Maison/:id' element={<House houses={houses} />} />
-
-              {/* Admin Routes */}
-              <Route path='/admin' element={<PrivateRoute role={ADMIN} />}>
-                <Route
-                  path='dashboard'
-                  element={<Admin houses={houses} bookings={bookings} />}
-                />
-                <Route path='maison/ajouter' element={<AddNewHouse />} />
-              </Route>
-            </Routes>
-          </Global>
-
+              <Route path='maison/ajouter' element={<AddNewHouse />} />
+            </Route>
+          </Routes>
           <Footer />
         </UserContextProvider>
       </>
