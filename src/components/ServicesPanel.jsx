1cbf43d62d3d30d696b7360286d33ab9153e6749
Maxime MEDAL <medal.max@gmail.com>
2022-01-28 00:08:54 +0100

    adjust card

--- src/components/ServicesPanel.jsx
+++ src/components/ServicesPanel.jsx
@@ -1,55 +1,55 @@
-import { PropTypes } from 'prop-types';
-import styled from 'styled-components';
-import colors from './styled-components/colors';
+import { PropTypes } from "prop-types";
+import styled from "styled-components";
+import colors from "./styled-components/colors";
 
-function ServicesPanel ({service}) {
-    ServicesPanel.propTypes = {
-            service: PropTypes.string.isRequired,
-        }
+function ServicesPanel({ service }) {
+  ServicesPanel.propTypes = {
+    service: PropTypes.string.isRequired,
+  };
 
-    return (
-        <>
-            <ServiceCard>
-                <Image src={service.img_url}/>
-             <TitleSpan>{service.name}</TitleSpan>
-            <ServicesDescription>{service.description}</ServicesDescription>
-            </ServiceCard>
-        </>
-    );
-    };
+  return (
+    <>
+      <ServiceCard>
+        <Image src={service.img_url} />
+        <TitleSpan>{service.name}</TitleSpan>
+        <ServicesDescription>{service.description}</ServicesDescription>
+      </ServiceCard>
+    </>
+  );
+}
 
-    const ServiceCard = styled.div`
-        display: flex;
-        flex-direction: column;
-        border-radius: 15px;
-        background-color: ${colors.lightGreen};
-        margin: 1.7rem;
-        height: 25rem;
-        box-shadow: 10px 10px 15px;
-        width: 30%;
+const ServiceCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-radius: 15px;
+  background-color: ${colors.lightGreen};
+  margin: 1.7rem;
+  height: 25rem;
+  box-shadow: 10px 10px 15px;
+  width: 25%;
 
-        @media (max-width: 768px){
-            width: 90%;
-        }
+  @media (max-width: 768px) {
+    width: 90%;
+  }
 `;
 
 const Image = styled.img`
-    border-top-left-radius: 15px;
-    border-top-right-radius: 15px;
-    height: 60%;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+  height: 60%;
 `;
 
 const TitleSpan = styled.span`
-    font-size: 1.5rem;
-    font-weight: bold;
-    color: ${colors.green};
-    text-align: center;
-    margin-top: 20px;
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: ${colors.green};
+  text-align: center;
+  margin-top: 20px;
 `;
 
 const ServicesDescription = styled.p`
-    text-align: center;
-    margin-top: 20px;
+  text-align: center;
+  margin-top: 20px;
 `;
 
 export default ServicesPanel;
