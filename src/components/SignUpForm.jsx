89cab1c2fb6df615fc3be6fcde189ad5407891da
MalikCherfi <malikcherfi@gmail.com>
2022-02-08 16:41:29 +0100

    header page inscription

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -7,6 +7,7 @@ import TitleForm from "./styled-components/TitleForm";
 import ContainerForm from "./styled-components/ContainerForm";
 import Submitbutton from "./styled-components/SubmitButton";
 import createUsers from "../data/createUsers";
+import Navigation from "./Navigation";
 
 function SignUpForm() {
   const { register, handleSubmit } = useForm();
@@ -25,112 +26,115 @@ function SignUpForm() {
   };
 
   return (
-    <MainContainer>
-      <ContainerForm marginTop="8%" marginBottom="8%">
-        <TitleForm>Créer un compte</TitleForm>
-        <Form onSubmit={handleSubmit(onSubmit)}>
-          <label htmlFor="genre">
-            <select {...register("genre", { required: true })}>
-              <option value="Mme">Madame</option>
-              <option value="Mr">Monsieur</option>
-            </select>
-          </label>
-          <br />
-          <label htmlFor="lastname">
-            Nom* : <br />
-            <input
-              type="text"
-              placeholder="Nom"
-              {...register("lastname", { required: true })}
-            />
-          </label>
-          <br />
-          <label htmlFor="firstname">
-            Prénom* : <br />
-            <input
-              type="text"
-              placeholder="Prénom"
-              {...register("firstname", { required: true })}
-            />
-          </label>
-          <br />
-          <label htmlFor="birth_date">
-            Date de naissance* : <br />
-            <input
-              type="date"
-              placeholder="Date de naissance"
-              {...register("birth_date", { required: true })}
-            />
-          </label>
-          <br />
-          <label htmlFor="email">
-            Email* : <br />
-            <input
-              type="email"
-              placeholder="Email"
-              {...register("email", { required: true })}
-            />
-          </label>
-          <br />
-          <label htmlFor="adress">
-            Adresse* : <br />
-            <input
-              type="textarea"
-              placeholder="Adresse"
-              {...register("adress", { required: true })}
-            />
-          </label>
-          <br />
-          <label htmlFor="zipcode">
-            Code postal* : <br />
-            <input
-              type="text"
-              placeholder="Code postal"
-              {...register("zipcode", {
-                required: true,
-              })}
-            />
-          </label>
-          <br />
-          <label htmlFor="city">
-            Ville* : <br />
-            <input
-              type="text"
-              placeholder="Ville"
-              {...register("city", { required: true })}
-            />
-          </label>
-          <br />
-          <label htmlFor="phone">
-            Téléphone* : <br />
-            <input
-              type="tel"
-              placeholder="Téléphone"
-              {...register("phone", { required: true })}
-            />
-          </label>
-          <br />
-          <label htmlFor="password">
-            Mot de passe* : <br />
-            <input
-              type="password"
-              placeholder="Mot de passe"
-              {...register("password", {
-                required: true,
-                minLength: {
-                  value: 8,
-                  message: "Password must have at least 8 characters",
-                },
-              })}
-            />
-          </label>
-          <br />
+    <>
+      <Navigation />
+      <MainContainer>
+        <ContainerForm marginTop="8%" marginBottom="8%">
+          <TitleForm>Créer un compte</TitleForm>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor="genre">
+              <select {...register("genre", { required: true })}>
+                <option value="Mme">Madame</option>
+                <option value="Mr">Monsieur</option>
+              </select>
+            </label>
+            <br />
+            <label htmlFor="lastname">
+              Nom* : <br />
+              <input
+                type="text"
+                placeholder="Nom"
+                {...register("lastname", { required: true })}
+              />
+            </label>
+            <br />
+            <label htmlFor="firstname">
+              Prénom* : <br />
+              <input
+                type="text"
+                placeholder="Prénom"
+                {...register("firstname", { required: true })}
+              />
+            </label>
+            <br />
+            <label htmlFor="birth_date">
+              Date de naissance* : <br />
+              <input
+                type="date"
+                placeholder="Date de naissance"
+                {...register("birth_date", { required: true })}
+              />
+            </label>
+            <br />
+            <label htmlFor="email">
+              Email* : <br />
+              <input
+                type="email"
+                placeholder="Email"
+                {...register("email", { required: true })}
+              />
+            </label>
+            <br />
+            <label htmlFor="adress">
+              Adresse* : <br />
+              <input
+                type="textarea"
+                placeholder="Adresse"
+                {...register("adress", { required: true })}
+              />
+            </label>
+            <br />
+            <label htmlFor="zipcode">
+              Code postal* : <br />
+              <input
+                type="text"
+                placeholder="Code postal"
+                {...register("zipcode", {
+                  required: true,
+                })}
+              />
+            </label>
+            <br />
+            <label htmlFor="city">
+              Ville* : <br />
+              <input
+                type="text"
+                placeholder="Ville"
+                {...register("city", { required: true })}
+              />
+            </label>
+            <br />
+            <label htmlFor="phone">
+              Téléphone* : <br />
+              <input
+                type="tel"
+                placeholder="Téléphone"
+                {...register("phone", { required: true })}
+              />
+            </label>
+            <br />
+            <label htmlFor="password">
+              Mot de passe* : <br />
+              <input
+                type="password"
+                placeholder="Mot de passe"
+                {...register("password", {
+                  required: true,
+                  minLength: {
+                    value: 8,
+                    message: "Password must have at least 8 characters",
+                  },
+                })}
+              />
+            </label>
+            <br />
 
-          <Submitbutton type="submit">Envoyer</Submitbutton>
-          <ToastContainer />
-        </Form>
-      </ContainerForm>
-    </MainContainer>
+            <Submitbutton type="submit">Envoyer</Submitbutton>
+            <ToastContainer />
+          </Form>
+        </ContainerForm>
+      </MainContainer>
+    </>
   );
 }
 
