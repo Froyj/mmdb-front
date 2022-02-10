bb16b140c825f398b2140242439aeca2d520a56d
Antoniatch <antoniatch@zohomail.eu>
2022-02-10 14:35:24 +0100

    update house page

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,79 +1,21 @@
-import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { PropTypes } from "prop-types";
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState } from "react";
 
 import Modal from 'react-modal';
 import FilledButton from "./styled-components/FilledButton";
-import FoodOption from "./FoodOption";
 import colors from "./styled-components/colors";
 
-function BookingForm({ house }) {
-  BookingForm.propTypes = {
-    house: PropTypes.string.isRequired,
-  };
+function BookingForm() {
 
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => {
-    // eslint-disable-next-line no-console
-    console.log(data);
-  };
-
-  const [mealOptions, setMealOptions] = useState([]);
-  const [ show, setShow ] = useState("true");
   const [openModal, setOpenModal] = useState(false);
   
-  useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_API_URL}/options`)
-      .then((response) => {
-        setMealOptions(response.data);
-      });
-    }, []);
-
   return (
     <Container>
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        
-        <h1>Réservez votre maison</h1>
-        
-        <QuickBooking>
-          <SelectBookingDates>
-            <label htmlFor="arrival">
-              Arrivée
-              <br />
-              <input type="date" {...register("arrival")} />
-            </label>
-            <label htmlFor="departure">
-              Départ
-              <br />
-              <input type="date" {...register("departure")} />
-            </label>
-          </SelectBookingDates>
-          <SelectTravellersNumber>
-            <label htmlFor="travellersNumber">
-              <p> Nombre de voyageurs </p>
-              <input
-                type="number"
-                {...register("travellersNumber")}
-                min="1"
-                max={house.capacity}
-              /> 
-            </label>
-          </SelectTravellersNumber>
-        </QuickBooking>
-        
-        <OptionChoice>
-        <Options>Choix des options</Options>
-        <TitleOption onClick={() =>setShow(!show)}><li>La carte</li></TitleOption>
-        {show ? null : mealOptions
-        .map((repas) => <FoodOption key={repas.id} repas={repas} />)}
-        </OptionChoice>
-        
+      <Form >
+        <h1>Tarifs</h1>
+                
         <PriceDetails>
-          <SumUpTitle>Résumé de votre réservation</SumUpTitle>
           <SumUp>
             <p>Prix x Nombre de nuits</p>
             <p>Frais de ménage (60,00€)</p>
@@ -159,7 +101,6 @@ width: 33%;
 `
 
 const Form = styled.div`
-  /* border: 2px solid #1c2c46; */
   background-color: ${colors.green};
   padding: 1rem;
   border-radius: 15px;
@@ -177,127 +118,7 @@ const Form = styled.div`
     padding: 20px 20px;
     color: white
   }
-`;
-
-const QuickBooking = styled.div`
-  border: 3px solid white;
-  width: 95%;
-  border-radius: 10px;
-  display: flex;
-  flex-direction: column;
-  margin: auto;
-  margin-top: 10px;
-`;
-
-const SelectBookingDates = styled.div`
-display: flex;
-flex-direction: row;
-justify-content: center;
-font-size: 1.2rem;
-
-label {
-  width: 45%;
-  text-align: center;
-  padding: .5rem
-}
-
-input {
-  width: 90%;
-  height: 1.5rem;
-  border-radius: 10px;
-  border: none;
-  text-align: center;
-  font-size: .9rem
-}
-
-  @media (max-width: 768px) {
-    display: flex;
-    flex-direction: column;
-    
-    label {
-      display: flex;
-      flex-direction: row;
-      font-size: 1rem;
-      width: 80%
-    }
-
-    input {
-      margin: 0 1rem
-    }
-    
-  }
-`;
-
-const SelectTravellersNumber = styled.div`
-  display: flex;
-  flex-direction: row;
-  border-radius: 5px;
-  width: 100%;
-
-  label {
-    display: flex;
-    flex-direction: row;
-    margin: .5rem 0;
-
-    p{
-      padding: 0 1rem;
-      font-size: 1.2rem;
-      color: white
-    }
-
-    input {
-      width: 15%;
-      height: 1.3rem;
-      border-radius: 10px;
-      padding: 0 .6rem
-    }
-  }
-
-  @media (max-width: 768px) {
-    label {
-      p {
-        font-size: 1rem;
-        padding: 0
-      }
-    }
-
-    input {
-      margin: 0 1rem
-    }
-  }
 `
-
-const OptionChoice = styled.div`
-background-color: white;
-border-radius: 10px;
-padding: 1rem;
-width: 89%;
-margin: auto;
-margin-top: 1.5rem;
-
-li {
-  line-height: 40px;
-}
- `
-
-const Options = styled.h3`
-text-align: center;
-`
-
-const TitleOption = styled.h2`
-  margin-top: 1rem;
-  width: auto;
-  list-style: none;
-  cursor: pointer;
-  
-  &:hover {
-    color: #ba9b5c;
-  }
-`;
-
-const SumUpTitle = styled.h3`
-text-align: center;
-`;
 
 const SumUp = styled.div`
   width: 100%;
