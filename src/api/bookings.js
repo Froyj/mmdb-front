9f939ab6378f428c2f232dacfa0df20dd66c638e
Geoffroy Periot <geoffroy.periot.dev@gmail.com>
2022-03-23 08:46:40 +0100

    booking list for user ok

--- src/api/bookings.js
+++ src/api/bookings.js
@@ -1,11 +1,14 @@
 import axios from '../helper/axios-config';
 
 const getBookings = (setVar) => {
-    axios
+  axios
     .get('/bookings')
     .then((response) => response.data)
     .then((data) => setVar(data))
-    .catch((err) => console.log(err))
+    .catch((err) => console.log(err));
 };
 
-export default getBookings;
+const getUserBookings = (userId) =>
+  axios.get(`/bookings?user=${userId}`).then((response) => response.data);
+
+export { getBookings, getUserBookings };
